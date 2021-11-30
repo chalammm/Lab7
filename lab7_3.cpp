aaa8f55216b6784f4078ba0d45d4aa8c492fb24e
chalammm <tipanan_ji@cmu.ac.th>
2021-11-30 17:31:53 +0700

    completed lab7_3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,40 @@
 
 using namespace std;
 
+double adiff(int x,int y)
+{
+  if(x>=360 or x <= -360)
+  {
+    x = x % 360;
+  }
+  if(y>=360 or y <= -360)
+  {
+    y = y % 360;
+  }
+  int z;
+  if(x>=y)
+  {
+    z = x-y;
+  }else
+  {
+    z = y-x;
+  }
+  if(z>180)
+  {
+    z = 360 - z; 
+  }
+  return z ;
+}
 
-int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+int main()
+{
+  cout << adiff(180,270) << "\n";
+  cout << adiff(210,45) << "\n";
+  cout << adiff(0,360) << "\n";
+  cout << adiff(10,350) << "\n";
+  cout << adiff(95,260) << "\n";
+  cout << adiff(90,-90) << "\n"; 
+  cout << adiff(1000,280) << "\n";
+  cout << adiff(60,244) << "\n";
+  retrun 0;
 }
